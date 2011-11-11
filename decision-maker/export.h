1385590949b02b2b1f737eddf523b86b952c3d3b
borislav nikolov <vf1100c@gmail.com>
2011-11-11 08:16:13 +0200

    update comment about example of reply function

--- decision-maker/export.h
+++ decision-maker/export.h
@@ -39,79 +39,14 @@ void p_cleanup(void);
 /*
 
 example of p_reply function:
- ECOPY(global.my_mac,reply_packet->p.eh.ether_shost);
- reply_packet->p.eh.ether_type = htons(ETHERTYPE_IP);
-
- if (request_packet->d.flags != 0)
-	ECOPY(bc_mac,reply_packet->p.eh.ether_dhost);
- else 
-	ECOPY(request_packet->p.eh.ether_shost,reply_packet->p.eh.ether_dhost); 
- 
- 
- struct ip *ip=(struct ip *) &reply_packet->p.ip;
- ip->ip_v = 4;
- ip->ip_hl = 20/4;
- ip->ip_tos = IPTOS_LOWDELAY;
- ip->ip_len = htons(sizeof(*reply_packet) - sizeof(struct ether_header));
- ip->ip_id = 0;
- ip->ip_off = 0;
- ip->ip_ttl = 0xF;
- ip->ip_p = IPPROTO_UDP;
- ip->ip_sum = 0;
- ip->ip_src = global.siaddr;
- struct udphdr *udp=(struct udphdr *) &reply_packet->p.udp;
- udp->uh_sport=htons(67);
- udp->uh_dport=htons(68);
- udp->uh_ulen=htons(sizeof(*reply_packet) - sizeof(struct ether_header) - sizeof(struct ip));
- udp->uh_sum=0;
- 
- reply_packet->d.op = BOOTREPLY;
- reply_packet->d.htype = 1; 
- reply_packet->d.hlen = ETHER_ADDR_LEN;
- reply_packet->d.hops = 0;
- reply_packet->d.xid = request_packet->d.xid;
- reply_packet->d.secs = 0;
- reply_packet->d.flags = request_packet->d.flags;
- reply_packet->d.ciaddr.s_addr = 0;
- reply_packet->d.yiaddr = l->ip;
- reply_packet->d.giaddr = request_packet->d.giaddr;
- bcopy(request_packet->d.chaddr,reply_packet->d.chaddr,16);
- 
- reply_packet->d.cookie = htonl(DHCP_COOKIE);
- 
- 
- if (request_packet->d.flags != 0) 
-	reply_packet->p.ip.ip_dst.s_addr = INADDR_BROADCAST; 
- else 
-	reply_packet->p.ip.ip_dst = l->ip;
- reply_packet->o.msg.code = 53;
- reply_packet->o.msg.len = 1;
- reply_packet->o.msg.msg_type = reply_msg_type;
- 
- reply_packet->o.server_id.code = 54;
- reply_packet->o.server_id.len = 4;
- reply_packet->o.server_id.addr = global.siaddr;
- reply_packet->o.end.code = 255;
- 
- 
- reply_packet->o.lease_time.code = 51;
- reply_packet->o.lease_time.len = 4;
- reply_packet->o.lease_time.lease_time = htonl(l->lease_time);
- reply_packet->o.subnet.code = 1;
- reply_packet->o.subnet.len = 4;
- reply_packet->o.subnet.addr = l->mask;
- reply_packet->o.broadcast.code = 28;
- reply_packet->o.broadcast.len = 4;
- reply_packet->o.broadcast.addr = l->br;
- 
- reply_packet->o.gateway.code = 3;
- reply_packet->o.gateway.len = 4;
- reply_packet->o.gateway.addr = l->gw;
-
- reply_packet->o.dns.code = 6;
- reply_packet->o.dns.len = sizeof(struct in_addr) * DNS_COUNT;
- int i;
- for (i=0;i<DNS_COUNT;i++) {
-	reply_packet->o.dns.addr[i] = l->dns[i];
- }
-  */
+ struct options *o = &reply_packet->o;
+ o_add_u8(o,O_MSGTYPE,reply_msg_type);
+ o_add_u32(o,O_SRV_ID,global.siaddr.s_addr);
+ o_add_u32(o,O_LEASE_TIME,htonl(l->lease_time));
+ o_add_u32(o,O_SUBNET,l->subnet.s_addr);
+ o_add_u32(o,O_BROADCAST,l->broadcast.s_addr);
+ o_add_u32(o,O_ROUTER,l->gw.s_addr);
+ o_add(o,O_DNS,4*(sizeof(l->dns)/sizeof(l->dns[0])),&l->dns[0]);
+ o_end(o);
+ fill_eth_ip_udp_dh_hdr(request_packet,reply_packet,l->ip);	
+*/
